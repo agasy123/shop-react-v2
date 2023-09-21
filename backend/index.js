8baad50c183c96b4755e99a0240bddfe66a0845d
agasy123 <agasyharut1234@gmail.com>
2023-09-21 19:26:50 +0400

    mysql password changed 2

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const localStorage=require("localStorage")
 var con = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "",
+  password: "tXrJRlp0J2bylo5ISOyQ",
   database: "node_project",
 });
 
