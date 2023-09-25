1aba2803b7bb7f96f85f22717edafdf0e579de3b
agasy123 <agasyharut1234@gmail.com>
2023-09-25 21:40:34 +0400

    added the port

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,9 @@ const localStorage=require("localStorage")
 var con = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "",
-  database: "node_project",
+  password: "PfN8KqAUtrdzuIAHBgBn",
+  port: "3306",
+  database: "agasy",
 });
 
 con.connect((err) => {
